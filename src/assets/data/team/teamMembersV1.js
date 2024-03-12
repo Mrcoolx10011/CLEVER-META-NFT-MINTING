1b51a11eb59f3c99b55721a10c50a1ca1b392839
unknown <2019intbtechcsekiranm@itmvu.in>
2024-03-12 21:33:03 +0530

    first commit

---
+++ src/assets/data/team/teamMembersV1.js
@@ -0,0 +1,149 @@
+import { FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
+
+import avatarThumb1 from "../../images/team/crew_avatar1.png";
+import avatarThumb2 from "../../images/team/crew_avatar2.png";
+import avatarThumb3 from "../../images/team/crew_avatar3.png";
+import avatarThumb4 from "../../images/team/crew_avatar4.png";
+import avatarThumb5 from "../../images/team/crew_avatar5.png";
+import avatarThumb6 from "../../images/team/crew_avatar6.png";
+import avatarThumb7 from "../../images/team/crew_avatar7.png";
+import avatarThumb8 from "../../images/team/crew_avatar8.png";
+
+const data = [
+  {
+    avatar: avatarThumb1,
+    name: "Kiran Mehta",
+    designation: "Founder",
+    socialLinks: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "https://www.linkedin.com/in/1kiranmehta/",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+      {
+        icon: <FaInstagram />,
+        url: "#",
+      },
+    ],
+  },
+ 
+  {
+    avatar: avatarThumb3,
+    name: "Monak Joshi",
+    designation: "Artist",
+    socialLinks: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+      {
+        icon: <FaInstagram />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    avatar: avatarThumb4,
+    name: "Kiran Mehta",
+    designation: "Co-Artist",
+    socialLinks: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+      {
+        icon: <FaInstagram />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    avatar: avatarThumb5,
+    name: "Jigar prajapati",
+    designation: "UI Designer",
+    socialLinks: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+      {
+        icon: <FaInstagram />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    avatar: avatarThumb6,
+    name: "Krishna Pandya",
+    designation: "Software Engineer",
+    socialLinks: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+      {
+        icon: <FaInstagram />,
+        url: "#",
+      },
+    ],
+  },
+  // {
+  //   avatar: avatarThumb7,
+  //   name: "",
+  //   designation: "Blockchain Dev",
+  //   socialLinks: [
+  //     {
+  //       icon: <FaLinkedinIn />,
+  //       url: "#",
+  //     },
+  //     {
+  //       icon: <FaTwitter />,
+  //       url: "#",
+  //     },
+  //     {
+  //       icon: <FaInstagram />,
+  //       url: "#",
+  //     },
+  //   ],
+  // },
+  {
+    avatar: avatarThumb8,
+    name: "Yash",
+    designation: "SEO Engineer",
+    socialLinks: [
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+      {
+        icon: <FaInstagram />,
+        url: "#",
+      },
+    ],
+  },
+];
+
+export default data;
